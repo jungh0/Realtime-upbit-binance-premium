09364b75888cb4fb715f4a16a33a948c5ee6b952
Jung <iveinvalue@gmail.com>
2019-08-15 16:19:00 +0900

    remove_https

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,37 @@
 var express = require('express');
 var router = express.Router();
-var http = require('https');
-var options = {
-    hostname: 'm.kr.investing.com',
-    path: '/currencies/usd-krw'
-};
+// var http = require('https');
+// var options = {
+//     hostname: 'm.kr.investing.com',
+//     path: '/currencies/usd-krw'
+// };
 
-var USDKRW = "0"
+// var USDKRW = "0"
 
-function handleResponse(response) {
-  var serverData = '';
-  response.on('data', function (chunk) {
-    serverData += chunk;
-  });
-  response.on('end', function () {
-    console.log("received server data:");
-    var result = serverData.split('<span class="lastInst pid-650-last">')[1].split('</span>')[0]
-    var tmp = result.trim();
-    console.log(tmp);
-    USDKRW = tmp;
-  });
-}
+// function handleResponse(response) {
+//   var serverData = '';
+//   response.on('data', function (chunk) {
+//     serverData += chunk;
+//   });
+//   response.on('end', function () {
+//     console.log("received server data:");
+//     var result = serverData.split('<span class="lastInst pid-650-last">')[1].split('</span>')[0]
+//     var tmp = result.trim();
+//     console.log(tmp);
+//     USDKRW = tmp;
+//   });
+// }
 
-function reloadusdkrw(){
-  http.request(options, function(response){
-    handleResponse(response)
-  }).end();
-  return USDKRW
-}
+// function reloadusdkrw(){
+//   http.request(options, function(response){
+//     handleResponse(response)
+//   }).end();
+//   return USDKRW
+// }
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { usdkrw: USDKRW });
+  res.render('index', { usdkrw: 'usdkrw' });
 });
 
 
